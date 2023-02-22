3dc420d31756b3a9774b48da29ba9e8bb4ba8a80
Zhanerrrkeee <124174188+Zhanerrrkeee@users.noreply.github.com>
2023-02-22 21:12:40 +0700

    Add files via upload

---
+++ lab4/src/app/products.ts
@@ -0,0 +1,112 @@
+export interface Product {
+  id: number;
+  name: string;
+  price: number;
+  description: string;
+  URL: string;
+  img:string;
+  rating:number;
+}
+
+export const products = [
+  {
+    id: 1,
+    name: 'Iphone 14 pro',
+    price: 631200 ,
+    description: 'NFC technology: Yes',
+    img:"https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h4d/h62/62948396728350/apple-iphone-14-pro-128gb-fioletovyj-106363319-1.jpg",
+    URL:"https://kaspi.kz/shop/p/apple-iphone-14-pro-256gb-fioletovyi-106363319/?c=750000000#!/item",
+    rating:4.3
+  },
+  {
+    id: 2,
+    name: 'Thermomix TM6',
+    price: 885000,
+    description: 'Power: 1500W ,bowl capacity: 2.2 l',
+    img:"https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/hb4/ha8/33399278174238/thermomix-tm6-belyj-101073636-1-Container.jpg",
+    URL:"https://kaspi.kz/shop/p/thermomix-tm6-belyi-101073636/?c=750000000#!/item",
+    rating:4.3
+  },
+  {
+    id: 3,
+    name: 'KENWOOD KCL95.004SI Cooking Chef XL',
+    price: 649990,
+    description: 'number of speeds: 8',
+    URL:"https://kaspi.kz/shop/p/kenwood-kcl95-004si-cooking-chef-xl-serebristyi-100883789/?c=750000000#!/item",
+    img:"https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h00/hc8/33543472480286/kenwood-kcl95-004si-cooking-chef-xl-serebristyj-100883789-1-Container.jpg",
+    rating:4.3
+  },
+  {
+    id: 4,
+    name: 'Ноутбук Acer Nitro 5 AN515-57 NH.QEKER.004 черный',
+    price: 389897 ,
+    description: 'screen diagonal: 15.6 inches',
+    URL:"https://kaspi.kz/shop/p/acer-nitro-5-an515-57-nh-qeker-004-chernyi-108194028/?c=750000000#!/item",
+    img:"https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h2e/hc4/67236399185950/acer-nitro-5-an515-57-nh-qeker-004-chernyi-108194028-1.jpg",
+    rating:4.3
+  },
+  {
+    id: 5,
+    name: 'Transformers Comfort Dining Table 1 300x100x78 cm',
+    price: 77499,
+    description: 'type: transformer',
+    URL:"https://kaspi.kz/shop/p/transformers-komfort-1-300x100x78-sm-104220696/?c=750000000#!/item",
+    img:"https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h9e/h05/49553540939806/transformers-komfort-1-300x100x78-sm-104220696-1.jpg",
+    rating:4.3
+  },
+  {
+    id: 6,
+    name: 'LG TV 50UQ76003LD 127 cm Black',
+    price: 277840,
+    description: 'Type: LED TV, diagonal: 50 inch',
+    URL:"https://kaspi.kz/shop/p/lg-50uq76003ld-127-sm-chernyi-105054596/?c=750000000#!/item",
+    img:"https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h5f/h35/51110128386078/lg-50uq76003ld-chernyi-105054596-1.jpg",
+    rating:4.3
+  },
+  {
+    id: 7,
+    name: 'Soft market elec 2 sofa, velour upholstery, 70x285x70, ivory',
+    price: 168970,
+    description: 'upholstery: velour',
+    URL:"https://kaspi.kz/shop/p/soft-market-eleg2-divan-obivka-veljur-70x285x70-slonovaja-kost--105351299/?c=750000000#!/item",
+    img:"https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h14/h68/51626435969054/soft-market-elegans-eleg2-obivka-velur-70x285x70-slonovaa-kost-105351299-1.jpg",
+    rating:4.3
+  },
+  {
+    id: 8,
+    name: 'Apple iPhone 13 128Gb White Smartphone',
+    price: 372870,
+    description: 'NFC technology: Yes',
+    URL:"https://kaspi.kz/shop/p/apple-iphone-13-128gb-belyi-102298420/?c=750000000#!/item",
+    img:"https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h96/h3c/46392664227870/apple-iphone-13-128gb-belyj-102298420-1-Container.jpg",
+    rating:4.3
+  },
+  {
+    id: 9,
+    name: 'Smart Speaker Yandex Station Mini 2 with Clock Black',
+    price:44500,
+    description: 'total capacity: 10 watts',
+    URL:"https://kaspi.kz/shop/p/jandeks-stantsija-mini-2-s-chasami-chernyi-102580021/?c=750000000#!/item",
+    img:"https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h4c/hb7/46546983649310/yandeks-novaya-stantsiya-mini-s-chasami-chernyi-102580021-1.jpg",
+    rating:4.3
+  },
+  {
+    id: 10,
+    name: 'Apple iPad 2021 10.2 64Gb Wi-Fi Tablet Grey',
+    price:178470,
+    description: 'diagonal: 10.2 inches',
+    URL:"https://kaspi.kz/shop/p/apple-ipad-2021-10-2-64gb-wi-fi-seryi-102301598/?c=750000000#!/item",
+    img:"https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/ha9/h54/48110926659614/apple-ipad-2021-wi-fi-64gb-seryj-102301598-1-Container.jpg",
+    rating:4.3
+  },
+   
+   
+   
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
